70c238a8ea19d442d00d2d410cb8e37672613cc0
Chala Tesfaye <chalatesfaye11@gmail.com>
2024-07-31 15:50:53 +0000

    How to remove locked file and push again

--- learnpython/lockedfile.py
+++ learnpython/lockedfile.py
@@ -1,2 +1,6 @@
 
-#How_unlock_locked_file in the code bash write this, rm -f /workspace/your-project-directory/.git/index.lock
+#How_remove_locked_file in the code bash write this, rm -f /workspace/your-project-directory/.git/index.lock
+# after remove, it needs again add 
+# git add filename
+# git commit -m "whrite message"
+# git push origin main/or actual name of your branch 
