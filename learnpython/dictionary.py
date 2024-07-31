7d54fc39ab2eb5b874df4ec8180bd9df833dd2e4
Chala Tesfaye <chalatesfaye11@gmail.com>
2024-07-31 15:33:54 +0000

    dictionary.py file

--- learnpython/dictionary.py
+++ learnpython/dictionary.py
@@ -1,3 +1,10 @@
-a= dict(Y=1, X=-1)
-b= {"Y":1, "X":-1}
-print(a,b)
+# a= dict(Y=1, X=-1)
+# b= {"Y":1, "X":-1}
+# print(a,b)
+
+customer1= {"id": "123a", "Name": "Chala Tesfaye"}
+customer2= {"id":"123b","Name":"Bahru Desalegn"}
+customes =[customer1, customer2]
+# print(customes)
+customers=(customer1, customer2)
+print(customers)
