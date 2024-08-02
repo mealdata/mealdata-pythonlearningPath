3704dd1574d79a58c0af0d0e449f1c3ad8a79704
Chala Tesfaye <chalatesfaye11@gmail.com>
2024-08-02 07:15:15 +0000

    if_elif taxes calculation file

---
+++ learnpython/if_elif.py
@@ -0,0 +1,14 @@
+# if, elif specialize for taxes pay
+
+income = 36600
+if income<10000:
+    tax_cofficient = 0.0 #1
+elif income<30000:
+    tax_cofficient= 0.2 #2
+elif income < 100000:
+    tax_cofficient = 0.35 #3
+else:
+    tax_cofficient = 0.45 # 4
+print("I will pay", income*tax_cofficient, "in taxe" )
+
+
