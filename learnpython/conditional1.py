0c980422d8f6f7635f09a6e1fd1688a22bf2e2c6
Chala Tesfaye <chalatesfaye11@gmail.com>
2024-08-02 07:12:41 +0000

    Boolen condtional file

---
+++ learnpython/conditional1.py
@@ -0,0 +1,7 @@
+# condtional-1
+late= False
+
+if late: # else part to be printed, becouse late condering as True. 
+    print("I will cal for manager ")
+else:
+     print("I will not call for manager")
