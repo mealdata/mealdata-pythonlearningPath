94fdd3e53175a5c44e190c9d9acb18cd83982c64
Chala Tesfaye <chalatesfaye11@gmail.com>
2024-08-03 15:14:26 +0000

    Simple for loop file

---
+++ learnpython/simple_for.py
@@ -0,0 +1,8 @@
+ #simple_for.py file 
+surnames = ['Chala', 'Abdu', 'Selam']
+for position in range(len(surnames)):
+ print(position, surnames[position])
+ """This is simple sequence of for list shows positions and surnames list, the print will be 
+ 0 Chala
+ 1 Abdu
+ 2 Selam """
