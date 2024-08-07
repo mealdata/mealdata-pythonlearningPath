607e3a6af54521d75dc2ab39a139eb782f357218
Chala Tesfaye <chalatesfaye11@gmail.com>
2024-08-07 18:35:50 +0000

    How to create python venv

---
+++ learnpython/createpythonenv.py
@@ -0,0 +1,6 @@
+# T create python working enviromment and project 
+# $ python -m venv myenv(or you can write what you want to call your working env)
+# to create the project, first open the working virtual env by using source myenv/bin/activate or 
+# myenv\Scripts\activate
+# then now the time is to create the project 
+# mkdir myproject/project name that we want call it, for example the mine is learnpython
---
+++ learnpython/simple_for1.py
@@ -0,0 +1,9 @@
+ #simple_for.py file 
+surnames = ['Chala', 'Abdu', 'Selam']
+for position in surnames:
+ print(surnames)
+ """This is simple sequence of for list shows positions and surnames list, the print will be 
+  Chala Abdu Selam
+  Chala Abdu Selam
+  Chala Abdu Selam
+  """
