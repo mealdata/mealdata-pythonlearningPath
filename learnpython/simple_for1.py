6e62b9af792ec04e3f1d5fbb06264581babbe365
Chala Tesfaye <chalatesfaye11@gmail.com>
2024-08-07 18:53:28 +0000

    Simple for loop file

--- learnpython/simple_for1.py
+++ learnpython/simple_for1.py
@@ -3,7 +3,7 @@ surnames = ['Chala', 'Abdu', 'Selam']
 for position in surnames:
  print(surnames)
  """This is simple sequence of for list shows positions and surnames list, the print will be 
-  Chala Abdu Selam
-  Chala Abdu Selam
-  Chala Abdu Selam
+  Chala , Abdu , Selam
+  Chala , Abdu , Selam
+  Chala , Abdu , Selam
   """
