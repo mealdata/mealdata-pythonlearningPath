43673ec10298fff7967142b12d787aac90858a4f
Chala Tesfaye <chalatesfaye11@gmail.com>
2024-08-08 17:22:30 +0000

    Simple list Indexing value file

---
+++ learnpython/list.py
@@ -0,0 +1,11 @@
+countries =[ "Kenya", "Ethiopia", "Uganda", "Nigeria", "Mali", "South Sudan"]
+
+print(countries)
+
+# indexing the string 
+print(countries[0])
+print(countries[:2])
+print(countries[2:])
+print(countries[0:4])
+countries[0] = "South Africa" # here the Kenya will replaced by South Africa
+print(countries)
