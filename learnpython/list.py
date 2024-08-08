52b611adddbc1a291deb8451d8f7633fa4d17eed
Chala Tesfaye <chalatesfaye11@gmail.com>
2024-08-08 18:02:54 +0000

    Simple list file

--- learnpython/list.py
+++ learnpython/list.py
@@ -1,5 +1,5 @@
 countries =[ "Kenya", "Ethiopia", "Uganda", "Nigeria", "Mali", "South Sudan"]
-
+countries2 =list(("SouthAfrica ", "Amerca", "Jerman", "Awropa", "Talian", "Djiabout"))
 print(countries)
 
 # indexing the string 
@@ -9,3 +9,17 @@ print(countries[2:])
 print(countries[0:4])
 countries[0] = "South Africa" # here the Kenya will replaced by South Africa
 print(countries)
+print(type(countries2))
+print(type(countries))
+
+countries.extend(countries2)
+
+print(countries)
+
+countries.remove('Mali')
+
+countries.append("USA")
+
+print(countries)
+
+# to clear all list use, countries.clear()
