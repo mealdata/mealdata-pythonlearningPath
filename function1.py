cf7f61e16892daea9f43d45cc39dbcfdeb780f87
Chala Tesfaye <chalatesfaye11@gmail.com>
2024-08-09 19:51:42 +0000

    "Excel and function1 files"

---
+++ function1.py
@@ -0,0 +1,8 @@
+def Datainfo(name, age):
+     print(f'Hello {name} well come to oursite, your age is {age}')
+name = str(input("Please input your name: "))
+age = int(input("What is your age: "))
+
+
+
+Datainfo(name, age)
