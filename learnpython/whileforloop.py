ae280dc5ac611687101dad79c83cc4c4e6bdec95
Chala Tesfaye <chalatesfaye11@gmail.com>
2024-08-10 13:33:03 +0000

    While and for loop file

---
+++ learnpython/whileforloop.py
@@ -0,0 +1,14 @@
+# i=1
+# while i<10:
+#     print(i)
+#     i+=1
+
+# x= range(4)
+# for y in x:
+#     print(y)
+#     print(x)
+
+mylist= [1,2,3,4,5]
+for m in mylist:
+    print(m)
+    print(mylist)
