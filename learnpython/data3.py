ac1449d26da80f7dd354824298e4b338268ebe42
Chala Tesfaye <chalatesfaye11@gmail.com>
2024-08-23 12:24:02 +0000

    Open csv, txt file in python

---
+++ learnpython/data3.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+
+data = pd.read_excel('ECC-BaseLine-Survey_Phase2-2024.xlsx')
+
+print(data)
+
+# to open  csv and txt file
+
+data1= pd.read_csv("filename.csv")
+# open txt file in python
+
+data2 = pd.read_txt ("filename.txt")
+
